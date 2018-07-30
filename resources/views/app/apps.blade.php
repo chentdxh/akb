d32772420df5c3932e8052d968d50d26161bbe01
fatih <fatih@qq.com>
2018-07-30 20:50:55 +0800

    Updat

--- resources/views/app/apps.blade.php
+++ resources/views/app/apps.blade.php
@@ -76,34 +76,26 @@
 
 
 
-    <div class="modal fade" id="addAppUserDialog">
+    <div class="modal fade" id="addAppUserDialog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Default Modal</h4>
+                    <h4 class="modal-title">添加App用户</h4>
                 </div>
                 <div class="modal-body">
                     <form>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
+                            <label for="exampleInputEmail1">uid</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputFile">File input</label>
-                            <input type="file" id="exampleInputFile">
-                            <p class="help-block">Example block-level help text here.</p>
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox"> Check me out
-                            </label>
-                        </div>
+
+
                         <button type="submit" class="btn btn-default">Submit</button>
                     </form>
                 </div>
@@ -118,7 +110,7 @@
     </div>
     <!-- /.modal -->
 
- 
+
 @stop
 
 @section('css')
