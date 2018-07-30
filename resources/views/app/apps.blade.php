3cedceadf3e9a368e93c885f5f47a8fcb356abb0
fatih <fatih@qq.com>
2018-07-30 21:14:45 +0800

    Updat

--- resources/views/app/apps.blade.php
+++ resources/views/app/apps.blade.php
@@ -64,9 +64,8 @@
                                 Action <span class="caret"></span>
                             </button>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
+                                <li><a href="/app/users?appid={{$appInfo->appid}}">add User</a></li>
+
                                 <li role="separator" class="divider"></li>
                                 <li><a href="#">Separated link</a></li>
                             </ul>
