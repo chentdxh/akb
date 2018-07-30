c5bd41ee19f936755a18b056344c933deae1b489
fatih <fatih@qq.com>
2018-07-30 21:18:33 +0800

    Updat

--- resources/views/app/users.blade.php
+++ resources/views/app/users.blade.php
@@ -87,7 +87,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-primary" v-on:click="add_user">Save</button>
                 </div>
             </div>
             <!-- /.modal-content -->
@@ -114,7 +114,7 @@
     <script >
         const app = new Vue({ el: '#app',
             methods:{
-                add_user:function(token)
+                add_user:function()
                 {
                     $.ajax({
                         url:"/serve/app/user/add",
