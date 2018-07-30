4908065ad4a304447c6e814b3a26b082176b6e5d
fatih <fatih@qq.com>
2018-07-30 21:13:42 +0800

    Updat

--- resources/views/app/apps.blade.php
+++ resources/views/app/apps.blade.php
@@ -58,16 +58,19 @@
                     <td>{{$appInfo->created_at}}</td>
                     <td>
 
-                        <div class="dropdown">
-                            <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown trigger
-                                <span class="caret"></span>
+                        <!-- Single button -->
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Action <span class="caret"></span>
                             </button>
-                            <ul class="dropdown-menu" aria-labelledby="dLabel">
-                                <li>Add User</li>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
                             </ul>
                         </div>
-
                     </td>
                 </tr>
                 @endforeach
