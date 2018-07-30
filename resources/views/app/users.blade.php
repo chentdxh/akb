cc930c290d92b69e32d2858075c1eabd5208e04e
fatih <fatih@qq.com>
2018-07-30 21:18:54 +0800

    Updat

--- resources/views/app/users.blade.php
+++ resources/views/app/users.blade.php
@@ -87,7 +87,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" v-on:click="add_user">Save</button>
+                    <button type="button" class="btn btn-primary" v-on:click="add_user" data-dismiss="modal">Save</button>
                 </div>
             </div>
             <!-- /.modal-content -->
