5deb9a5bc83e65fa35b901b4f7f87c9be3f8796e
fatih <fatih@qq.com>
2018-07-30 16:12:37 +0800

    Updat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GameMan
                 </div>
 
                 <div class="links">
