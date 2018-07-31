9d347c2d1e3a0f86d9efc808b7f4f223a77bd670
fatih <fatih@qq.com>
2018-07-31 18:36:39 +0800

    Updat

--- resources/views/aichat/apps.blade.php
+++ resources/views/aichat/apps.blade.php
@@ -20,7 +20,7 @@
 @section('content')
     <div class="box">
         <div class="box-header with-border">
-            <h3 class="box-title">App列表</h3>
+            <h3 class="box-title">AiChat App列表</h3>
 
             <div class="box-tools">
 
