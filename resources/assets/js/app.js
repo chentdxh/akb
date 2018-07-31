165ae9d7d42037c76d584465dd9be339d9ce1fa5
fatih <fatih@qq.com>
2018-07-31 14:25:19 +0800

    Updat

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,14 +10,11 @@
 require('./bootstrap');
 require("jquery-slimscroll");
 window.Vue = require('vue');
-
 window.toastr = require('toastr');
-
-
 window.swal = require('sweetalert2');
 
-var Promise = require('promise-polyfill').default;
-require("underscore");
+// var Promise = require('promise-polyfill').default;
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -35,12 +32,47 @@ require("underscore");
 // });
 
 
-import { show_delete_dialog } from './custom.js';
 
+window.show_delete_dialog = function (url,data) {
 
+    swal({
+        title: '确定删除？',
+        text: "删除后不可恢复",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText:"取消",
+        confirmButtonText: '确认'
+    }).then((result) => {
+        if (result.value) {
 
+            $.ajax({
+                url:url,
+                data:data,
+                type:"post",
+                success:function (response) {
 
-window.show_delete_dialog = show_delete_dialog; 
+                    if (response.code == 0)
+                    {
+                        swal({type:"success",title:"删除成功!" }).then((result) => {
+
+                            window.location.reload();
+                        });
+                    }else
+                    {
+                        swal({type:"error",title:"删除失败!" })
+                    }
+                },
+                error:function (response) {
+
+                    swal({type:"error",title:"删除失败!" })
+                }
+            });
+
+        }
+    })
+}
 
 
 window.show_success_dialog = function(title,callback)
