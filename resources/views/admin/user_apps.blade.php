329d2e45ccb1a833bfeb8ec495e52ba03b3e9961
fatih <fatih@qq.com>
2018-08-13 14:04:21 +0800

    Updat

--- resources/views/admin/user_apps.blade.php
+++ resources/views/admin/user_apps.blade.php
@@ -21,12 +21,12 @@
         <!-- /.box-header -->
         <div class="box-body">
 
-            <select id='userApps' multiple='multiple'>
+            <select multiple="multiple" id="my-select" name="my-select[]">
                 <option value='elem_1'>elem 1</option>
                 <option value='elem_2'>elem 2</option>
                 <option value='elem_3'>elem 3</option>
                 <option value='elem_4'>elem 4</option>
-
+                ...
                 <option value='elem_100'>elem 100</option>
             </select>
 
@@ -107,7 +107,7 @@
 
 
         $(document).ready(function(){
-            $("#userApps").multiSelect();
+            $('#my-select').multiSelect()
         })
 
 
