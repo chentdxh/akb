f11c4105a746ae35eae20aacf47188ce04d15f5b
fatih <fatih@qq.com>
2018-08-13 15:17:21 +0800

    Updat

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,11 +24,11 @@ class AdminController extends Controller
     public function user_apps(Request $request)
     {
         $uid = $request->input("uid");
-        $user = User::where('uid',$uid)->first();
-        $data['user'] = empty($uid)?$this->get_user():$user ;
+        $user =  empty($uid)?$this->get_user():User::where("uid",$uid)->first() ;
+        $data['user'] = $user;
 
 
-        $data['user_apps'] = UserApp::where("uid",$uid)->get();
+        $data['user_apps'] = UserApp::where("uid",$user->uid)->get();
         $data['all_apps'] = AppInfo::get();
 
         return view("admin.user_apps",$data);
