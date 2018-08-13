286daa5958ea6358f2422d86480ee6955ed2db5e
fatih <fatih@qq.com>
2018-08-13 14:20:35 +0800

    Updat

--- resources/views/admin/user_apps.blade.php
+++ resources/views/admin/user_apps.blade.php
@@ -61,8 +61,7 @@
             $('#my-select').multiSelect({
                 selectableHeader: "<div class='custom-header'>Selectable items</div>",
                 selectionHeader: "<div class='custom-header'>Selection items</div>",
-                selectableFooter: "<div class='custom-header'>Selectable footer</div>",
-                selectionFooter: "<div class='custom-header'>Selection footer</div>"
+               
             })
         })
 
