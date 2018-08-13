7792953768b5c3c0ac7e7c3bad0151917c02c3a0
fatih <fatih@qq.com>
2018-08-13 12:12:51 +0800

    Updat

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\UserApp;
 
 class AdminController extends Controller
 {
---
+++ database/migrations/2018_08_03_174458_create_applications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("aid")->unique();
+            $table->string("name");
+            $table->string("desc")->nullable();
+            $table->string("status")->nullable();
+            $table->string("uid");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
