cc2f7f90698650ed72b4514e19efa05c0e8e20be
fatih <fatih@qq.com>
2018-08-13 14:09:32 +0800

    Updat

--- resources/views/admin/user_apps.blade.php
+++ resources/views/admin/user_apps.blade.php
@@ -91,12 +91,11 @@
 @section('css')
     <link rel="stylesheet" href="/css/custom.css">
 
-    <link rel="stylesheet" href="/libs/multiselect/css/multi-select.css">
+
 @stop
 
 @section('js')
-
-    <script   src="/libs/multiselect/js/jquery.multi-select.js" />
+ 
 @stop
 
 
