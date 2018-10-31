d7645ea47874186e53cc9000fef5eb60ae394f3f
Arthur <arthur@Arthurs-Mac-mini.local>
2018-10-31 19:35:49 +0800

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use App\User;
 use Auth;
+use App\FileInfo;
 
 class Controller extends BaseController
 {
