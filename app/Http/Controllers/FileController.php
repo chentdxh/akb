ba4db20d468d958f593ed57186973d31fea63b0e
Arthur <arthur@Arthurs-Mac-mini.local>
2018-10-31 11:13:06 +0800

    update

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FileInfo;
+use App\Http\Traits\FileTrait;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    //
+    use FileTrait;
+
+
+    public function upload(Request $request)
+    {
+
+        $disk = "data";
+
+        $file = $request->file('file');
+
+        $fileHash = hash_file('md5', $file->path());
+
+        $rst = $this->save_to_disk($disk);
+
+
+        if (!empty($result)) {
+
+
+            $fileInfo = new FileInfo();
+
+            $fileInfo->fid = uniqid("f");
+
+            $fileInfo->mime_type = $rst['mime_type'];
+
+            $fileInfo->path = $rst['path'];
+
+            $fileInfo->name = $rst['name'];
+            $fileInfo->file = $rst['file'];
+
+            $fileInfo->hash = $fileHash;
+
+            $fileInfo->save();
+
+            return $this->json_return(0, "success", $fileInfo);
+
+        } else {
+            return $this->json_return(-1, "upload file failed");
+        }
+    }
+}
