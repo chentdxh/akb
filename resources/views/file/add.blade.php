a7305419f2000d01319aa71ba14e9c6aa6e16d22
arthur <fatih@qq.com>
2018-11-13 13:20:18 +0800

    update

--- resources/views/file/add.blade.php
+++ resources/views/file/add.blade.php
@@ -73,7 +73,7 @@
         <div class="box-footer text-center">
             <a href="#!" class=" " id="uploadFileBtn">上传</a>
 
-            <a href="#!" class=" " id="uploadBtn">上传(文件名id)</a>
+            <a href="#!" class=" " id="uploadBtn">上传</a>
             <a href="#!" id="uploadTencentBtn">上传腾讯云</a>
             <a href="#!" id="uploadAliyunBtn">上传阿里云</a>
         </div>
@@ -164,9 +164,8 @@
         $(function () {
 
 
-            create_uploader("#uploadFileBtn", "/data/file/upload?idtype=file");
+            create_uploader("#uploadFileBtn", "/data/file/upload");
 
-            create_uploader("#uploadBtn", "/data/file/upload");
 
             create_uploader("#uploadBtn", "/data/file/upload?idtype=file");
 
